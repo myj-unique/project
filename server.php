1caae2ecb260a3f8ff7c114d601cb27ffb416522
myj-unique <59025273+myj-unique@users.noreply.github.com>
2019-12-19 00:28:24 +0800

    swoole-init
    swoole-init

---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+$serv = new Swoole\Server("127.0.0.1",9501);
+
+$serv->on('Connect',function($serv,$fd){
+    echo "Client:Connect.\n";
+});
+
+$serv->on('Receive',function($serv,$fd, $from_id, $data){
+    $serv->send($fd,'Server: '.$data);
+});
+
+$serv->on('Close',function($serv, $fd){
+    echo "Client:Close.\n";
+});
+
+$serv->start();
